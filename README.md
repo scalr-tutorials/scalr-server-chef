94338fa1a2afbce23fb27faa11b09ac36f42a677
lcc2207 <lynn@fidmail.com>
2018-03-16 15:34:31 -0500

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# scalr-server
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: scalr-server
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
